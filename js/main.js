6288c86f43a81cd9a3838023d315f3677a8bc677
Rtrunzo <richtrunzo@gmail.com>
2020-12-02 11:20:26 -0800

    Assigned the forms values to the profile data model when the form is submitted. Also updated HTML classes on the form inputs. #1.

--- js/main.js
+++ js/main.js
@@ -8,7 +8,17 @@ $avatarInput.addEventListener('input', function (event) {
 });
 
 var $form = document.querySelector('form');
+var $username = document.querySelector('.username');
+var $fullName = document.querySelector('.full-name');
+var $location = document.querySelector('.location');
+var $bio = document.querySelector('.bio');
 
 $form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  data.profile.username = $username.value;
+  data.profile.fullName = $fullName.value;
+  data.profile.location = $location.value;
+  data.profile.avatarUrl = $avatarInput.value;
+  data.profile.bio = $bio.value;
 
 });
