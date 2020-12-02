9e3673520bcd1417b39479716686ef0fb951ddb0
Rtrunzo <richtrunzo@gmail.com>
2020-12-02 10:51:13 -0800

    Updated the avatar url to change the src when a url is inputed into the form field. Also updated css to create consitent image sizing when a new image is inputted into the avatar url form. #1.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 var $avatarInput = document.querySelector('.avatar');
+var $imagePlaceholder = document.querySelector('.image-placeholder');
 
 $avatarInput.addEventListener('input', function (event) {
+  var $avatarValue = $avatarInput.value;
+  $imagePlaceholder.src = $avatarValue;
 
 });
