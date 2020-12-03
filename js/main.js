45c7288e4059ad824601c3f0e8e145620609faa5
Rtrunzo <richtrunzo@gmail.com>
2020-12-02 17:16:22 -0800

    Created a function that returns a DOM tree that matches the profile HTML. #2.

--- js/main.js
+++ js/main.js
@@ -22,3 +22,64 @@ $form.addEventListener('submit', function (event) {
   $form.reset();
   $imagePlaceholder.src = 'images/placeholder-image-square.jpg';
 });
+
+function profileData(event) {
+  var profile = document.createElement('div');
+  var header = document.createElement('header');
+  profile.appendChild(header);
+  var codeJournal = document.createElement('h3');
+  var codeJournalText = document.createTextNode('Code Journal');
+  codeJournal.appendChild(codeJournalText);
+  header.appendChild(codeJournal);
+  var rowOne = document.createElement('div');
+  rowOne.setAttribute('class', 'row-one row-one-profile');
+  profile.appendChild(rowOne);
+  var colOne = document.createElement('div');
+  colOne.setAttribute('class', 'col-one-row-one');
+  rowOne.appendChild(colOne);
+
+  var profileName = document.createElement('h3');
+  profileName.setAttribute('class', 'profile-header');
+
+  colOne.appendChild(profileName);
+
+  var image = document.createElement('img');
+  image.setAttribute('class', 'image-placeholder profileimage');
+
+  colOne.appendChild(image);
+  var colTwo = document.createElement('div');
+  colTwo.setAttribute('class', 'col-two-row-one');
+  rowOne.appendChild(colTwo);
+
+  var profileUser = document.createElement('div');
+  profileUser.setAttribute('class', 'profileuser');
+  colTwo.appendChild(profileUser);
+  var userIcon = document.createElement('i');
+  userIcon.setAttribute('class', 'far fa-user');
+  profileUser.appendChild(userIcon);
+
+  var username = document.createElement('p');
+  profileUser.appendChild(username);
+
+  var profileLocation = document.createElement('div');
+  profileLocation.setAttribute('class', 'profilelocation');
+  colTwo.appendChild(profileLocation);
+
+  var locationIcon = document.createElement('i');
+  locationIcon.setAttribute('class', 'fas fa-compass');
+  profileLocation.appendChild(locationIcon);
+
+  var locationName = document.createElement('p');
+  profileLocation.appendChild(locationName);
+
+  var profileBio = document.createElement('div');
+  profileBio.setAttribute('class', 'profilebio');
+  colTwo.appendChild(profileBio);
+
+  var bioText = document.createElement('p');
+  profileBio.appendChild(bioText);
+
+  return profile;
+}
+
+profileData();
